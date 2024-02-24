7a01be1a955c405cc99f2708ee4e35b6515fd851
Manjula36 <manjulavdt@gmail.com>
2024-02-24 19:58:30 +0530

    Add project files.

---
+++ BankLoginAndDashboardPage/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BankLoginAndDashboardPage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BankLoginAndDashboardPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ BankLoginAndDashboardPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankLoginAndDashboardPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
